638ea48eb240a5cb39053e87117bcb9d40b7c778
jgarces1231 <josegarces@colegioguanenta.edu.co>
2024-03-01 08:55:20 -0500

    se realizo analisis y diseño

--- ejercicio_2/ejercicio2.py
+++ ejercicio_2/ejercicio2.py
@@ -1,12 +1,24 @@
+# programa para solicitar un prestamo 
+
 #input
-salario_emp=int(input("Digite el salario del empleado: "))
-deudas=input("El empleado tiene deudas: si , no?: ")
 
-#proceso y salida 
-if salario_emp>= 945200:
-    rta = "APROBADO :D"
+salario_emp=int(input("Digite su ingreso mensual: "))
 
+deudas=input("¿El empleado tiene deudas? si/no: ")
+
+#proceso 
+
+if salario_emp > 945200:
+
+    if deudas == "no":
+
+        rta = "APROBADO :D"
+        
+    else:
+        rta = "DENEGADO :("
 else:
-    rta = "RECHAZADO :("
-#output 
+
+    rta = "DENEGADO :("
+#output
+
 print("Su prestamo ha sido, " + rta)
